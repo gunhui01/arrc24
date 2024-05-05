70d8c6e57148709e12492e84df39fa5b63a79242
Park Gunhui <gunhui@pusan.ac.kr>
2024-05-05 18:53:07 +0900

    Exception handling

--- LiDAR/lidar_visualizer.py
+++ LiDAR/lidar_visualizer.py
@@ -15,29 +15,29 @@ lidar = RPLidar("/dev/ttyUSB0")
 canvas = np.zeros((CANVAS_SIZE, CANVAS_SIZE, 3), dtype=np.uint8)
 
 async def draw_circles(scan):
-    for (_, angle, distance) in scan:
+    for _, angle, distance in scan:
         x = int(np.sin(np.radians(angle)) * (distance * SCALE) + HALF_CANVAS_SIZE)
         y = int(np.cos(np.radians(angle)) * (distance * SCALE) + HALF_CANVAS_SIZE)
 
         cv2.circle(canvas, (x, y), 3, (0, 0, 255), -1)
 
-try:
+if __name__ == "__main__":
+    print("Press Ctrl+C to quit")
     while True:
-        health = lidar.get_health()
-        if health[0] == "Good":
+        try:
             for scan in lidar.iter_scans():
                 asyncio.run(draw_circles(scan))
                 flip_canvas = cv2.flip(canvas, 0)
                 cv2.imshow("LiDAR Visualizer", flip_canvas)
                 cv2.waitKey(1)
                 canvas = np.zeros((CANVAS_SIZE, CANVAS_SIZE, 3), dtype=np.uint8)
-        else:
-            print("Error code", health[1])
+        except KeyboardInterrupt:
+            lidar.stop()
+            lidar.disconnect()
+            cv2.destroyAllWindows()
+            break
+        except Exception:
             lidar.reset()
-except KeyboardInterrupt:
-    lidar.stop()
-    lidar.disconnect()
-    cv2.destroyAllWindows()
 
 """
 try:
