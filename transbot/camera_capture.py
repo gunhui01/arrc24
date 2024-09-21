b60a84ca435f86955c5cb531ab59e1859ee84bc6
staccato43 <staccato43@gmail.com>
2024-09-21 23:35:21 +0900

    Refactor camera_capture function to improve readability and error handling

--- transbot/camera_capture.py
+++ transbot/camera_capture.py
@@ -7,40 +7,42 @@ import subprocess, sys, cv2
 from time import sleep
 
 def camera_capture(frame_queue):
-    # 연결된 카메라 인덱스 확인
     try:
-        result = subprocess.run(['v4l2-ctl', '--list-devices'], stdout=subprocess.PIPE)
-        output = result.stdout.decode()
-        camera_name = "USB Camera2"  # 찾고자 하는 카메라의 이름
-        camera_index = None
+        # 연결된 카메라 인덱스 확인
+        try:
+            result = subprocess.run(['v4l2-ctl', '--list-devices'], stdout=subprocess.PIPE)
+            output = result.stdout.decode()
+            camera_name = "USB Camera2"  # 찾고자 하는 카메라의 이름
+            camera_index = None
 
-        lines = output.splitlines()
-        for i, line in enumerate(lines):
-            if camera_name in line:
-                device_line = lines[i + 1].strip()  # 다음 줄의 공백 제거
-                camera_index = int(device_line.replace('/dev/video', ''))
+            lines = output.splitlines()
+            for i, line in enumerate(lines):
+                if camera_name in line:
+                    device_line = lines[i + 1].strip()  # 다음 줄의 공백 제거
+                    camera_index = int(device_line.replace('/dev/video', ''))
+                    break
+            print(f"{camera_name} found at /dev/video{camera_index}")
+        except Exception as e:
+            print(f"Camera not found, {e}")
+            sys.exit(1)
+
+        # 카메라 초기화
+        cap = cv2.VideoCapture(camera_index)
+
+        while True:
+            # 카메라에서 프레임 읽기
+            ret, frame = cap.read()
+            if not ret:
+                print("프레임을 읽을 수 없음")
                 break
-        print(f"{camera_name} found at /dev/video{camera_index}")
-    except Exception as e:
-        print(f"Camera not found, {e}")
-        sys.exit(1)
 
-    # 카메라 초기화
-    cap = cv2.VideoCapture(camera_index)
+            if not frame_queue.full():
+                frame_queue.put(frame)
 
-    while True:
-        # 카메라에서 프레임 읽기
-        ret, frame = cap.read()
-        if not ret:
-            print("프레임을 읽을 수 없음")
-            break
+            sleep(0.01)
 
-        if not frame_queue.full():
-            frame_queue.put(frame)
-
-        sleep(0.01)
-
-    cap.release()
+    finally:
+        cap.release()
 
 
 
