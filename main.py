bd6c2d32c9bead111076ff9273836f6251f72335
Park Gunhui <gunhui@pusan.ac.kr>
2024-09-22 00:24:27 +0900

    Refactor main function

--- main.py
+++ main.py
@@ -6,40 +6,67 @@ from multiprocessing import Process, Queue
 from transbot.bot_control import bot_control
 from transbot.camera_capture import camera_capture
 from transbot.line_tracing import line_tracing
+from transbot.avoid_trees import lidar_scan
 from transbot.avoid_trees import avoid_trees
 
 QUEUE_CHECK_INTERVAL = 0.01
+CAMERA_WAIT_INTERVAL = 1
 
 def main():
-    flag_queue = Queue()    # 다음 프로그램 진행 유무(Bool)
-    control_queue = Queue() # 모터의 속도, 각속도(Tuple)
-    pause_queue = Queue()   # 모터 정지 유무(Bool)
-    frame_queue = Queue()   # 카메라가 받은 이미지
+    try:
+        flag_queue = Queue()    # 다음 프로그램 진행 유무(Bool)
+        control_queue = Queue() # 모터의 속도, 각속도(Tuple)
+        pause_queue = Queue()   # 모터 정지 유무(Bool)
+        frame_queue = Queue()   # 카메라가 받은 이미지
+        lidar_queue = Queue()   # LiDAR가 받은 데이터
 
-    camera_capture_process = Process(target=line_tracing, args=(frame_queue,))
-    line_tracing_process = Process(target=line_tracing, args=(frame_queue, control_queue, flag_queue))
-    avoid_trees_process = Process(target=avoid_trees, args=(control_queue,))
+        camera_capture_process = Process(target=camera_capture, args=(frame_queue,))
+        line_tracing_process = Process(target=line_tracing, args=(frame_queue, control_queue, flag_queue))
+        lidar_scan_process = Process(target=lidar_scan, args=(lidar_queue,))
+        avoid_trees_process = Process(target=avoid_trees, args=(lidar_queue, control_queue))
 
-    camera_capture_process.start()
-    print("camera_capture started.")
+        camera_capture_process.start()
+        time.sleep(CAMERA_WAIT_INTERVAL)
+        print("camera_capture started.")
 
-    ###  LINE_TRACING  ###
+        ###  LINE_TRACING  ###
 
-    line_tracing_process.start()
-    print("line_tracing started.")
+        line_tracing_process.start()
+        print("line_tracing started.")
 
-    while True:
-        if not control_queue.empty():
-            line, angular = control_queue.get()
-            bot_control(line, angular)
+        while True:
+            if not control_queue.empty():
+                line, angular = control_queue.get()
+                bot_control(line, angular)
 
-        if not flag_queue.empty():
-            finish = flag_queue.get()
-            if finish:
-                line_tracing_process.terminate()
-                print("line_tracing finished.")
-                break
-        time.sleep(QUEUE_CHECK_INTERVAL)
+            if not flag_queue.empty():
+                finish = flag_queue.get()
+                if finish:
+                    line_tracing_process.terminate()
+                    print("line_tracing finished.")
+                    break
+            time.sleep(QUEUE_CHECK_INTERVAL)
+        
+        ###  AVOID_TREES  ###
+
+        lidar_scan_process.start()
+        print("lidar_scan started.")
+
+        avoid_trees_process.start()
+        print("avoid_trees started.")
+
+        while True:
+            if not control_queue.empty():
+                line, angular = control_queue.get()
+                bot_control(line, angular)
+            time.sleep(QUEUE_CHECK_INTERVAL)
+
+    finally:
+        camera_capture_process.terminate()
+        line_tracing_process.terminate()
+        lidar_scan_process.terminate()
+        avoid_trees_process.terminate()
+        bot_control(0, 0)
 
 if __name__ == "__main__":
     main()
--- transbot/avoid_trees.py
+++ transbot/avoid_trees.py
@@ -3,71 +3,43 @@
 __author__ = "Park Gunhui"
 __credits__ = ["Lee Donghyun", "Hwang Hyeonjun", "Noh Minhyeok"]
 
-import asyncio, math
-from Transbot_Lib import Transbot
+import math, time
 from rplidar import RPLidar
 
-TURN_RIGHT = False
-TURN_LEFT = False
-DIAGONAL_LENGTH = 250
-DETECT_RANGE = 150
-COS_MULTIPLY = 150
-LINE_SPEED = 10
-ANGULAR_SPEED = 100
-
-def car_motion(line, angular):
-    if abs(line) >= 5 or abs(angular) >= 10:
-        speed_l = line / 100.0
-        speed_a = -angular / 100.0
-        bot.set_car_motion(speed_l, speed_a)
-    else:
-        bot.set_car_motion(0, 0)
-
-def lidar_scan():
-    global TURN_RIGHT, TURN_LEFT
-    for new_scan, quality, angle, distance in lidar.iter_measures():
-        if distance:
-            if 315 <= angle < 360:
-                TURN_RIGHT = distance < (DIAGONAL_LENGTH + DETECT_RANGE + math.cos(math.radians(int(angle))) * COS_MULTIPLY)
-            elif 0 <= angle < 45:
-                TURN_LEFT = distance < (DIAGONAL_LENGTH + DETECT_RANGE + math.cos(math.radians(int(angle))) * COS_MULTIPLY)
-
-async def robot_control():
-    while True:
-        if TURN_LEFT and TURN_RIGHT:
-            car_motion(LINE_SPEED, 0)
-        elif TURN_RIGHT:
-            car_motion(LINE_SPEED, ANGULAR_SPEED)
-            await asyncio.sleep(0.025)
-        elif TURN_LEFT:
-            car_motion(LINE_SPEED, -ANGULAR_SPEED)
-            await asyncio.sleep(0.025)
-        else:
-            car_motion(LINE_SPEED, 0)
-        await asyncio.sleep(0.01)
-
-async def main():
-    control_task = asyncio.create_task(robot_control())
-    await control_task
-
-if __name__ == "__main__":
-    bot = Transbot()
-    lidar = RPLidar('/dev/ttyUSB0')
-
-    print(lidar.get_info())
-    print(lidar.get_health())
-
+def lidar_scan(lidar_queue):
     try:
-        lidar_scan_thread = asyncio.get_event_loop().run_in_executor(None, lidar_scan)
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        car_motion(0, 0)
+        DIAGONAL_LENGTH = 250
+        DETECT_RANGE = 150
+        COS_MULTIPLY = 150
+
+        lidar = RPLidar('/dev/ttyUSB0')
+
+        for new_scan, quality, angle, distance in lidar.iter_measures():
+            if distance:
+                if 315 <= angle < 360:
+                    turn_right = distance < (DIAGONAL_LENGTH + DETECT_RANGE + math.cos(math.radians(int(angle))) * COS_MULTIPLY)
+                elif 0 <= angle < 45:
+                    turn_left = distance < (DIAGONAL_LENGTH + DETECT_RANGE + math.cos(math.radians(int(angle))) * COS_MULTIPLY)
+            lidar_queue.put((turn_right, turn_left))
+    finally:
         lidar.stop()
         lidar.disconnect()
-        del bot
-    except Exception as e:
-        print(f"Error: {e}")
-        car_motion(0, 0)
-        lidar.stop()
-        lidar.disconnect()
-        del bot
+
+def avoid_trees(lidar_queue, control_queue):
+    LINE_SPEED = 10
+    ANGULAR_SPEED = 100
+
+    while True:
+        if not lidar_queue.empty():
+            turn_right, turn_left = lidar_queue.get()
+
+        if turn_right and turn_left:
+            control_queue.put((LINE_SPEED, 0))
+        elif turn_right:
+            control_queue.put((LINE_SPEED, ANGULAR_SPEED))
+            time.sleep(0.025)
+        elif turn_left:
+            control_queue.put((LINE_SPEED, -ANGULAR_SPEED))
+            time.sleep(0.025)
+        else:
+            control_queue.put((LINE_SPEED, 0))
