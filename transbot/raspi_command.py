5c74c98326201102e272e87a524def988d8facff
Park Gunhui <gunhui@pusan.ac.kr>
2024-10-27 17:25:12 +0900

    Fix MQTT error

--- transbot/raspi_command.py
+++ transbot/raspi_command.py
@@ -9,13 +9,13 @@ def on_connect(client, userdata, flags, rc):
 	if rc == 0: print("Connected success")
 	else: print("Bad connection returned code: ", rc)
 
-def on_publish(client, userdata, mid):
-	print(f"Message published. MID: {mid}")
+# def on_publish(client, userdata, mid):
+# 	print(f"Message published. MID: {mid}")
 
 def raspi_command(command_queue):
     client = mqtt.Client()
     client.on_connect = on_connect
-    client.on_publish = on_publish
+    # client.on_publish = on_publish
     client.connect("192.168.0.2", 1883, 60)
 
     while True:
